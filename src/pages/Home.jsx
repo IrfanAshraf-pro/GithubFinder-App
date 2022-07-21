08ae46506895979d1cae5d9404bd594df1644ee9
IrfanAshraf-pro <irfanashraf4090@gmail.com>
2022-07-21 01:04:03 -0700

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import UserResults from '../components/users/UserResults'
+import Usersearch from '../components/users/Usersearch'
+
+function Home() {
+  return (
+    <>
+      <Usersearch/>
+      <UserResults/>
+    </>
+  )
+}
+
+export default Home
