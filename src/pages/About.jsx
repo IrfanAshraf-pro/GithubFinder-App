6a84fef4386f111448d7b3f19f06aeca87a1dcfa
M Irfan Ashraf <81696562+IrfanAshraf-pro@users.noreply.github.com>
2023-02-02 17:06:11 +0500

    updated about.jsx description

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function About() {
   return (
     <div>
-      Aboyut
+      This is a simple app that lets user search for a specific user on github. lets him see his profile and repositories.
     </div>
   )
 }
